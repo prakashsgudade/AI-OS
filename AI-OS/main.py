4368cdc7101c7c3753283eccbd49aeb3425d28c0
prakashsgudade <prakashsgudade@gmail.com>
2026-07-21 19:19:54 +0530

    Implement user greeting and AI interaction loop
    Initialize AI system, load user data, and handle user interaction.

--- AI-OS/main.py
+++ AI-OS/main.py
@@ -1,1 +1,31 @@
+from startup import start
+from memory import load_user, save_user
+from engine import AIEngine
 
+start()
+
+user = load_user()
+
+if "name" not in user:
+
+    user["name"] = input("Tumhara naam kya hai : ")
+
+    save_user(user)
+
+name = user["name"]
+
+print("Welcome", name)
+
+ai = AIEngine(name)
+
+while True:
+
+    question = input(f"{name} : ")
+
+    answer = ai.process(question)
+
+    print("AI :", answer)
+
+    if question.lower() == "bye":
+
+        break
