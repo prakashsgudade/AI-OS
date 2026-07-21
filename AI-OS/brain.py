b4715551f17ad234809a3247b439f5594f5b3a29
prakashsgudade <prakashsgudade@gmail.com>
2026-07-21 19:18:14 +0530

    Update brain.py

--- AI-OS/brain.py
+++ AI-OS/brain.py
@@ -1,1 +1,30 @@
+from datetime import datetime
 
+def think(question, name):
+
+    question = question.lower().strip()
+
+    if question == "hello":
+
+        return f"Hello {name}"
+
+    elif question == "time":
+
+        return datetime.now().strftime("%I:%M %p")
+
+    elif question == "date":
+
+        return datetime.now().strftime("%d-%m-%Y")
+
+    elif question == "tum kaun ho":
+
+        return "Main AI OS hoon."
+
+    elif question == "bye":
+
+        return "Bye."
+
+    else:
+
+        return "Ye feature abhi develop ho raha hai."
+    
