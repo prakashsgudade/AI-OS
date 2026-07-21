4d8dcb2751faaff6e16807ce8c89b6bc99e8444f
prakashsgudade <prakashsgudade@gmail.com>
2026-07-21 19:17:13 +0530

    Update startup.py

--- AI-OS/startup.py
+++ AI-OS/startup.py
@@ -1,1 +1,10 @@
+from config import APP_NAME, VERSION, DEVELOPER
 
+def start():
+
+    print("=" * 50)
+    print(APP_NAME)
+    print("Version :", VERSION)
+    print("Developer :", DEVELOPER)
+    print("=" * 50)
+  
