6cb26653806df06a5988d2fc4e56889c17bcb89f
prakashsgudade <prakashsgudade@gmail.com>
2026-07-21 19:17:43 +0530

    Update memory.py

--- AI-OS/memory.py
+++ AI-OS/memory.py
@@ -1,1 +1,25 @@
+import json
+import os
 
+FILE = "database/users.json"
+
+def load_user():
+
+    if os.path.exists(FILE):
+
+        with open(FILE, "r", encoding="utf-8") as f:
+
+            try:
+                return json.load(f)
+
+            except:
+
+                return {}
+
+    return {}
+
+def save_user(data):
+
+    with open(FILE, "w", encoding="utf-8") as f:
+
+        json.dump(data, f, indent=4)
