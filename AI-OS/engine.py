7ac09d2a245e8335cbbf999447d71ebe54e7f0d0
prakashsgudade <prakashsgudade@gmail.com>
2026-07-21 19:18:49 +0530

    Update engine.py

--- AI-OS/engine.py
+++ AI-OS/engine.py
@@ -1,1 +1,11 @@
+from brain import think
 
+class AIEngine:
+
+    def __init__(self, name):
+
+        self.name = name
+
+    def process(self, question):
+
+        return think(question, self.name)
